e39f0fd3fddd620a31ca57143f6f9fd390b6b4e6
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-11-19 20:21:13 +0550

    Cloud_Q11: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 0 Manufacturer: intex Model: Cloud Q11 Script version: 2.1.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ omni_Cloud_Q11.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from Cloud_Q11 device
+$(call inherit-product, device/intex/Cloud_Q11/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := Cloud_Q11
+PRODUCT_NAME := omni_Cloud_Q11
+PRODUCT_BRAND := Intex
+PRODUCT_MODEL := Cloud Q11
+PRODUCT_MANUFACTURER := intex
